8c13395c2304ac63563b7f96431cf8d4d04039f2
lerry903 <lerryli@foxmail.com>
2018-07-14 23:10:41 +0800

    init

---
+++ src/main/java/com/lerry/swaggershowdoc/SwaggerShowDocApplication.java
@@ -0,0 +1,20 @@
+package com.lerry.swaggershowdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author LErry.li
+ * Date: 2018-07-14
+ * Time: 下午11：00：00
+ */
+@SpringBootApplication
+public class SwaggerShowDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerShowDocApplication.class, args);
+    }
+}
